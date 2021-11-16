c426227afbd9a884987823e3104196e00ef13530
Gabriel De Vuyst <gabriel.m.devuyst@gmail.com>
2021-11-16 10:45:13 +0100

    Adding username and amenities to the User and Space models, respectively

--- app/models/space.rb
+++ app/models/space.rb
@@ -2,6 +2,10 @@ class Space < ApplicationRecord
   belongs_to :user
   has_many :bookings
 
+  AMENITIES = ["lock", "monitor", "desk", "mouse", "keyboard", "mirror", "tea", "coffee", "massage table", "parking space", "snacks", "shower", "hairdryer", "vacuum cleaner", "yoga mats", "balcony", "rooftop", "music" "player", "toaster", "fridge", "oven"]
+
   validates :address, :description, :name, presence: true
   validates :price, numericality: true, presence: true
+  validates  :amenities, inclusion: { scope: AMENITIES,
+    message: "Choose your amenities"}
 end
