906df91443a0c882ae088ee79b0af18ad71bab8d
Matan Amidor <matan.amidor@gmail.com>
2021-11-16 11:44:23 +0100

    added styling to the map

--- app/controllers/spaces_controller.rb
+++ app/controllers/spaces_controller.rb
@@ -20,10 +20,5 @@ class SpacesController < ApplicationController
     lang = @map["features"][0]["center"][1]
 
     @map_url = "https://api.mapbox.com/styles/v1/mapbox/streets-v11/static/#{long},#{lang},11,0/500x300@2x?access_token=pk.eyJ1IjoibWFkbzEzIiwiYSI6ImNrdzF3dWxsNjBhNzUyb3BhZDk4bHI1ZW8ifQ.Y7Ctk5-sMRJf1VbQGa0Y_g"
-
-
-
-
-
   end
 end
