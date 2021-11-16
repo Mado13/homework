0c1db30667e76fc75fe2b9f8ad291c3a5074db07
Matan Amidor <matan.amidor@gmail.com>
2021-11-16 11:38:01 +0100

    map api added to show page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_16_092648) do
+ActiveRecord::Schema.define(version: 2021_11_16_092020) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,7 +48,6 @@ ActiveRecord::Schema.define(version: 2021_11_16_092648) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "amenities"
     t.index ["user_id"], name: "index_spaces_on_user_id"
   end
 
