d0011fc4435208c4b22444cd3e43f7f862b94f0b
Matan Amidor <matan.amidor@gmail.com>
2021-11-17 11:32:42 +0100

    Gecoding halfway

--- app/models/space.rb
+++ app/models/space.rb
@@ -1,4 +1,6 @@
 class Space < ApplicationRecord
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
   serialize :amenities
   belongs_to :user
   has_many :bookings
