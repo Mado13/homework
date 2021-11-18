c0e014a80283776d63384ce0b40bbd23d8b9d6b5
Aleksa Mikic <988gbb@gmail.com>
2021-11-18 17:38:03 +0100

    dsadas

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_17_221642) do
+ActiveRecord::Schema.define(version: 2021_11_17_132717) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -79,8 +79,6 @@ ActiveRecord::Schema.define(version: 2021_11_17_221642) do
     t.datetime "updated_at", precision: 6, null: false
     t.float "latitude"
     t.float "longitude"
-    t.string "start_date"
-    t.string "end_date"
     t.index ["user_id"], name: "index_spaces_on_user_id"
   end
 
